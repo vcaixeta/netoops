81cbc0b199fccc04ce0a3296fb8f580801ccf097
Vinicius Caixeta dos Santos <vdossantos@zendesk.com>
2018-02-09 20:42:02 +0000

    descriptions

--- Python-labs/check_acl_sync.py
+++ Python-labs/check_acl_sync.py
@@ -2,8 +2,14 @@
 from cli import cli
 from cli import configure
 from netmiko import ConnectHandler
-import difflib
-import re
+
+'''
+This script should run on the GustShell, it requires netmiko to be installed. This will check the local acls, the peer
+router ACls, compare the difference and generate a SysLog message in case they are out-of-sync.
+'''
+
+#Users hardcoded in code as this is just an example script. In production you may want to use
+#Some sort of Vault.
 
 username = "superuser"
 password = "superpass"
@@ -12,6 +18,7 @@ password = "superpass"
 def get_remote_acls():
 
 	remote_acls = ""
+	#Specify the ip for the pair Router
 	device = { 'device_type': 'cisco_ios', 'ip':'10.250.0.20', 'username': username,
                     'password': password}
 
@@ -39,7 +46,6 @@ def acl_diff(local_acls,remote_acls):
 				 """.format("\n".join(lines))
 
 	if len(local_acls) > len(remote_acls):
-		#diff = set(local_acls) - set(remote_acls)
 		diff = "ATTENTION. ACLs are not in Sync, local ACL has more entries than Peer."
 	elif len(remote_acls) > len(local_acls):
 		diff = "ATTENTION. ACLs are not in Sync, remote ACL has more entries than Peer."
@@ -71,7 +77,9 @@ def main():
 
 
 	diff = acl_diff(local_acls, remote_acls)
-	reschedule(30, diff)
+
+	#specify in seconds how long to wait between checks.
+	reschedule(1800, diff)
 
 
 if __name__ == "__main__":
